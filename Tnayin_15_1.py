b568240ae3731f6cf6121f2b64c069ea957de35e
Gagik010101 <116186054+Gagik010101@users.noreply.github.com>
2023-05-17 16:46:57 +0400

    Add files via upload

---
+++ Tnayin_15_1.py
@@ -0,0 +1,26 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print("The students name is ", self.name)
+        print("The students age is ", self.age)
+
+
+class Student(Person):
+    def __init__(self, name, age, section):
+        Person.__init__(self, name, age)
+        self.section = section
+
+    def student(self):
+        Person.display(self)
+        print("student in ", self.section, "section")
+
+
+name = input("input students Name:\t")
+age = input("input students age:\t")
+section = input("input students section:\t")
+S = Student(name, age, section)
+S.student()
+

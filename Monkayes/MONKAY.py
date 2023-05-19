42ce54e1f03d664dc85fcfe2d483662a3dc353c7
Gagik010101 <116186054+Gagik010101@users.noreply.github.com>
2023-05-19 22:19:29 +0400

    Add files via upload

---
+++ Monkayes/MONKAY.py
@@ -0,0 +1,13 @@
+from Fauna.ANIMAL import Animal
+class Monkay(Animal):
+    def __init__(self, type_of_monkey, array, age):
+        super().__init__("Monkay")
+        self.type_of_monkey = type_of_monkey
+        self.array = array
+        self.age = age
+    def infprmation(self):
+        return f"this is monkay his type is {self.type_of_monkey}\nhis array is {self.array}"
+
+
+    def about_himself(self):
+        return f"Hi I'm {self.name}\nIm {self.age} years old\n"
---
+++ Tnayin 16.1.py
@@ -0,0 +1,11 @@
+from Monkayes.MONKAY import Monkay
+from Monkayes.DONKAY import Donkay
+
+monkay = Monkay("bobuin", "15kg", "20")
+donkay = Donkay("gorilla", "160kg", "55")
+print(monkay)
+print(monkay.infprmation())
+print(monkay.about_himself())
+print(donkay)
+print(donkay.infprmation())
+print(donkay.about_himself())
